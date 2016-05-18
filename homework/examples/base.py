503749e8e5303f551fccb6bb2cc356e300576e90
Alexey Kuleshov <alexeykuleshov1990@gmail.com>
2016-05-18 10:32:26 +0200

    Basic script
    Basic script for example of the lake

--- homework/examples/base.py
+++ homework/examples/base.py
@@ -1,14 +1,7 @@
-"""
- This script is part of a study-project by TU-Dresden
- We try to investigate model-complexity with calculation time
- Base script 'Lake'
-
-"""
 import os
 import numpy as np
 import flopy.modflow as mf
 import shutil
-import sys
 
 workspace = os.path.join('ascii')
 
@@ -19,7 +12,7 @@ if os.path.exists(workspace):
 if not os.path.exists(workspace):
     os.makedirs(workspace)
 
-name = 'gridsize_performance_test'
+name = 'lake_base_example'
 
 # --- Setting up the parameters
 # Groundwater heads
@@ -49,17 +42,11 @@ bot = np.linspace(-H / NLay, -H, NLay)
 # Calculation of row-width and col-width
 delRow = delCol = L/(N-1)
 
-# Number of timePeriods
-nPer = 100
-
-# Set steady of all periods to false
-steady = np.zeros(100)
-
 # Instantiate the discretization object
-dis = mf.ModflowDis(ml, nlay=NLay, nrow=N, ncol=N, delr=delRow, delc=delCol, top=0.0, botm=bot, laycbd=0, lenuni=2, itmuni=4, steady=steady, nper=nPer, perlen=1)
+dis = mf.ModflowDis(ml, nlay=NLay, nrow=N, ncol=N, delr=delRow, delc=delCol, top=0.0, botm=bot, laycbd=0, lenuni=2, itmuni=4, steady=False, nper=100, perlen=1)
 
 # helping-variable
-NHalf = int((N-1)/2)
+NHalf = (N-1)/2
 
 # iBound-Configuration
 iBound = np.ones((NLay, N, N))
@@ -102,6 +89,3 @@ oc = mf.ModflowOc(ml)
 
 ml.write_input()
 ml.run_model()
-
-if os.path.exists(workspace):
-    shutil.rmtree(workspace)
